66607d3ec12b6a762a87b9cee6609aab5a314d0b
Nikita Sokolov <nsokolov0309@gmail.com>
2023-03-12 13:33:55 +0200

    add operations and hooks for authSlice and make reducer logOut

--- src/components/GoogleAuth/GoogleAuth.jsx
+++ src/components/GoogleAuth/GoogleAuth.jsx
@@ -1,16 +1,14 @@
 import { Outlet } from 'react-router';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { googleLogout, useGoogleLogin } from '@react-oauth/google';
-import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
-import { authLogin } from 'redux/authSlice/authSlice';
-import { selectAccessToken } from 'redux/authSlice/authSelectors';
+import { useDispatch } from 'react-redux';
+import { authLogin, authLogout } from 'redux/authSlice/authSlice';
+import { fetchGoogleAuth } from 'redux/authSlice/authOperations';
+import { useAuth } from 'hooks/useAuth';
 
 const GoogleAuth = () => {
   const dispatch = useDispatch();
-  const accessToken = useSelector(selectAccessToken);
-  //   const [user, setUser] = useState([]);
-  const [profile, setProfile] = useState([]);
+  const { accessToken, isLoggedIn, email, name } = useAuth;
 
   const login = useGoogleLogin({
     onSuccess: codeResponse => {
@@ -20,39 +18,30 @@ const GoogleAuth = () => {
   });
 
   useEffect(() => {
+    const controller = new AbortController();
+
     if (accessToken) {
-      axios
-        .get(
-          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${accessToken}`,
-          {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-              Accept: 'application/json',
-            },
-          }
-        )
-        .then(res => {
-          setProfile(res.data);
-        })
-        .catch(err => console.log(err));
+      dispatch(fetchGoogleAuth());
     }
-  }, [accessToken]);
+
+    return () => controller.abort();
+  }, [accessToken, dispatch]);
 
   const logOut = () => {
     googleLogout();
-    setProfile(null);
+    dispatch(authLogout());
   };
 
   return (
     <>
       <div>
         <h1>google</h1>
-        {profile ? (
+        {isLoggedIn ? (
           <div>
             {/* <img src={profile.picture} alt="user image" /> */}
             <h3>User Logged in</h3>
-            <p>Name: {profile.name}</p>
-            <p>Email Address: {profile.email}</p>
+            <p>Name: {name}</p>
+            <p>Email Address: {email}</p>
             <br />
             <br />
             <button onClick={logOut}>Log out</button>
