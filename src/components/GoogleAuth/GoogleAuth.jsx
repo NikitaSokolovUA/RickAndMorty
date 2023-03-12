64e0dc8b96b5348f5e063052c5a8a35fd1c6e1c2
Nikita Sokolov <nsokolov0309@gmail.com>
2023-03-12 12:52:04 +0200

    google testing

---
+++ src/components/GoogleAuth/GoogleAuth.jsx
@@ -0,0 +1,62 @@
+import { Outlet } from 'react-router';
+import React, { useState, useEffect } from 'react';
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
+
+const GoogleAuth = () => {
+  const [user, setUser] = useState([]);
+  const [profile, setProfile] = useState([]);
+
+  const login = useGoogleLogin({
+    onSuccess: codeResponse => setUser(codeResponse),
+    onError: error => console.log('Login Failed:', error),
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: 'application/json',
+            },
+          }
+        )
+        .then(res => {
+          setProfile(res.data);
+        })
+        .catch(err => console.log(err));
+    }
+  }, [user]);
+
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+  };
+
+  return (
+    <>
+      <div>
+        <h1>google</h1>
+        {profile ? (
+          <div>
+            {/* <img src={profile.picture} alt="user image" /> */}
+            <h3>User Logged in</h3>
+            <p>Name: {profile.name}</p>
+            <p>Email Address: {profile.email}</p>
+            <br />
+            <br />
+            <button onClick={logOut}>Log out</button>
+          </div>
+        ) : (
+          <button onClick={() => login()}>Sign in with Google 🚀 </button>
+        )}
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default GoogleAuth;
